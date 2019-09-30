2880d603cc041bd40e3adfb74471ac9dcbb5f7d9
Jan Co Chiong <jan.cochiong@gmail.com>
2019-09-30 16:17:35 -0400

    add stock components

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1> The Stocks Component </h1>
+    <div>
+        <app-stock v-for="stock in stocks" :key="stock.id">{stock}</app-stock>
+    </div>
 </template>
+
+<script>
+    import Stock from './Stock.vue'
+
+    export default {
+        data(){
+            return{
+                stocks: [
+                    {id: 1, name: 'BMW', price: 110},
+                    {id: 2, name: 'FIRE', price: 11.12},
+                    {id: 3, name: 'APHRIA', price: 8},
+                    {id: 4, name: 'Canopy', price: 33}
+                ]
+            }
+        },
+        components:{
+            appStock: Stock
+        }
+    }
+</script>
