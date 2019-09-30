34c6282e17cffc41a552cc3ea38fc3037e82e05b
Jan Co Chiong <jan.cochiong@gmail.com>
2019-09-30 16:57:14 -0400

    vuex setup for stocks component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,18 +8,18 @@
     import Stock from './Stock.vue'
 
     export default {
-        data(){
-            return{
-                stocks: [
-                    {id: 1, name: 'BMW', price: 110},
-                    {id: 2, name: 'FIRE', price: 11.12},
-                    {id: 3, name: 'APHRIA', price: 8},
-                    {id: 4, name: 'Canopy', price: 33}
-                ]
-            }
-        },
         components:{
             appStock: Stock
+        },
+        computed: {
+            stocks(){
+                return this.$store.getters.stocks
+            }
+        },
+        computed: {
+            stocks(){
+                return this.$store.getters.stocks
+            }
         }
     }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'BMW', price: 110},
+    {id: 2, name: 'FIRE', price: 11.12},
+    {id: 3, name: 'APHRIA', price: 8},
+    {id: 4, name: 'Canopy', price: 33}
+]
